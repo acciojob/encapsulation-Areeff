3e448aa7c8deb775b06c70d0a9f1393facf4d39b
Areeff <p.areeff786@gmail.com>
2023-05-11 17:00:31 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String [] args){
+        RWOnly obj=new RWOnly();
+        //when obj.name accesesd directly it is showing error.Because we can not access private members from other class.
+        obj.setName("Areeff");
+        System.out.println(obj.getName());
+    }
+
 }
